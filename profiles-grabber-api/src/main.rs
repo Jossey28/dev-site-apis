c598e96f6f9b175d40e47fd360d6bfb0fd41decf
Irene A <121013355+Jossey28@users.noreply.github.com>
2026-05-23 06:30:25 -0500

    Working "Hello World" w/ SwaggerUI

--- profiles-grabber-api/src/main.rs
+++ profiles-grabber-api/src/main.rs
@@ -1,7 +1,24 @@
+use tokio::net::TcpListener;
+use utoipa_axum::{
+    routes,
+    PathItemExt,
+    router::OpenApiRouter
+};
+use utoipa_swagger_ui::SwaggerUi;
 
-
+#[utoipa::path(get, path = "", responses((status = OK, body=str)))]
+async fn hello_world() -> &'static str {
+    return "Hello There!";
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let (router, api) = OpenApiRouter::new()
+        .routes(routes!(hello_world))
+        .split_for_parts();
+
+    let app = router.merge(SwaggerUi::new("/docs").url("/api-docs/openapi.json", api));
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
